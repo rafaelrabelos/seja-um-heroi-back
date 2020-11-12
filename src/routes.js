5500d4e87008fa42cffef2db720b4a61db1761e9
rafaelrabelos <rafael_rabelo@live.com>
2020-11-11 21:33:47 -0300

    Descreve a api na rota /

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,22 @@ const LoginController = require('./controllers/LoginController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.status(200).send({api: "SejaUmHeroi"}));
+routes.get('/', (req, res) => res.status(200).send({
+    info:{
+        apiName: "SejaUmHeroi",
+        apiHost: os.hostname() || "Não indefificado",
+        apiEndpoints:["/api"],
+        apiVer: "v1",
+        apiPort: process.env.API_PORT || "Não indefificado"
+    },
+    createdBy:[
+        "Rafael Rabelo",
+        "André Marinho Moreira",
+        "Gustavo Caldeira de Oliveira",
+        "Henrique Alves Kirshke",
+        "Lucas Stehling Lara Carvalho"
+    ]
+}));
 
 // Sessão e login
 routes.post('/cadastro', LoginController.create);
