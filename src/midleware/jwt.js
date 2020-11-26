5a94d97d51378cd3242f231e5d0d44d25332a668
Rafael Rabelo <rafael_rabelo@live.com>
2020-11-26 20:30:23 -0300

    refatora middleware JWT

--- src/midleware/jwt.js
+++ src/midleware/jwt.js
@@ -4,38 +4,47 @@ function valideAuthJWT (req, res) {
 
     const headerAuth = req.headers.authorization;
 
-    if(!headerAuth ){
-        res.status(401).send({ status : false, erros : [ `Token não informado.`] });
+    if(!isValidAthorization(headerAuth)){
+        res.status(401).send({ status : false, erros : [ `Token mal formado`] });
     }
 
-    const authParts = headerAuth.split(' ');
+    const [base, token]  = headerAuth.split(' ');
+
+    if(!isValidJWT(token)){
+        res.status(401).send({ status : false, erros : ["Token invalido."]});
+    }
+
+    return true;
+}
+
+function isValidAthorization(authorization){
+
+    if(!authorization){
+        return false;
+    }
+
+    const authParts = authorization.split(' ');
 
     if(!authParts === 2){
-        res.status(401).send({ status : false, erros : [ "Token não identificavel."]});
-    }
-
-    const [base, token]  = authParts;
-
-    if(!/^Bearer$/i.test(base)){
-        res.status(401).send({ status : false, erros : [ "Token não mal formado."]});
-    }
-
-    if(isValidJWT(token)){
+        return false;
+    }else if(authParts[0] !== 'Bearer' || !/^Bearer$/i.test(authParts[0])){
+        return false;
+    }else if(authParts[1] === ''){
+        return false;
+    }else{
         return true;
     }
-    res.status(401).send({ status : false, erros : ["Token invalido."]});
 }
 
 function isValidJWT(token){
 
-    jwt.verify(token, process.env.TOKEN_HASH, (err, decoded) => {
+    return jwt.verify(token, process.env.TOKEN_HASH, (err, decoded) => {
         if(err){
             return false;
         }
-        req.user = decoded.user;
-        return true;
-    })
 
+        return true;
+    });
 }
 
 function generateToken( data = {} ){
@@ -46,7 +55,18 @@ function generateToken( data = {} ){
 }
 
 function decodeJWT(jwt_token){
-    return jwt.decode(jwt_token)
+
+    if(!isValidAthorization(jwt_token)){
+        return false;
+    }
+
+    const [base, token]  = jwt_token.split(' ');
+
+    if(!isValidJWT(token)){
+        return false;
+    }
+
+    return jwt.decode(token);
 }
 
 module.exports = { valideAuthJWT, generateToken, decodeJWT }
