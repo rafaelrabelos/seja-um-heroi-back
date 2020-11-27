88ffe78c5235aa9b62b6f74387cb1b61ada02a22
Rafael Rabelo <rafael_rabelo@live.com>
2020-11-27 00:32:13 -0300

    Adiciona campo wiki para classes de pets

--- src/models/pet/classe.js
+++ src/models/pet/classe.js
@@ -3,6 +3,7 @@ const mongoose = require('../../database/connection');
 const ClassePetSchema = new mongoose.Schema({
     nome : { type : String, required : true },
     descricao : { type : String},
+    wiki_link : { type : String},
     criadoEm : { type : Date, default : Date.now },
     criadoPor : { type : mongoose.Schema.Types.ObjectId, ref : 'User' },
 });
