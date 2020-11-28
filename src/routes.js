0d641e17b0f9d3f967bad32c1ac544bcc9fe0f54
rabelotw <rafael.rabelo@thoughtworks.com>
2020-11-28 14:45:13 -0300

    Refatora rotas

--- src/routes.js
+++ src/routes.js
@@ -29,17 +29,17 @@ routes.get('/', (req, res) => res.status(200).send({
 routes.post('/login', LoginController.autentica);
 
 // Users
-routes.get('/user', (req, res) => secure.secureRoute(req, res, null, UserController.getUser));
-routes.get('/user/obtem', (req, res) => secure.secureRoute(req, res, {admin: true}, UserController.getUsers));
-routes.get('/user/obtem/:usuarioId', (req, res) => secure.secureRoute(req, res, {owner : true}, UserController.getUser));
-routes.post('/user/create', UserController.createUser);
+routes.get('/user', (req, res) => secure.secureRoute(req, res, null, UserController.getSelfUser));
+routes.get('/user/todos', (req, res) => secure.secureRoute(req, res, {admin: true}, UserController.getUsers));
+routes.get('/user/:usuarioId', (req, res) => secure.secureRoute(req, res, null, UserController.getUser));
+routes.get('/user/:usuarioId/pets', (req, res) => secure.secureRoute(req, res, null, UserController.getUserPets));
+routes.post('/user', UserController.createUser);
 routes.put('/user', (req, res) => secure.secureRoute(req, res, null, UserController.updateUser));
-routes.put('/user/update/:usuarioId', (req, res) => secure.secureRoute(req, res, {owner : true}, UserController.updateUser));
+routes.put('/user/:usuarioId', (req, res) => secure.secureRoute(req, res, {owner : true}, UserController.updateUser));
 routes.delete('/user/:usuarioId', (req, res) => secure.secureRoute(req, res, {system : true}, UserController.deleteUser));
-routes.delete('/user/remove/:usuarioId', (req, res) => secure.secureRoute(req, res, {system : true}, UserController.deleteUser));
 
 // Pets
-routes.get('/pet/classe',  (req, res) => secure.secureRoute(req, res, null, PetController.getPetClasses));
-routes.post('/pet/classe', (req, res) => secure.secureRoute(req, res, {admin: true}, PetController.insertPetClasse));
+routes.get('/pet-classe',  (req, res) => secure.secureRoute(req, res, null, PetController.getPetClasses));
+routes.post('/pet-classe', (req, res) => secure.secureRoute(req, res, {admin: true}, PetController.insertPetClasse));
 
 module.exports = routes; 
